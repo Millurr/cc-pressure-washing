0aeb201ae9c89192e8c7ed16b9df2e2270adbeae
Millurr <42452986+Millurr@users.noreply.github.com>
2021-05-18 23:57:09 -0500

    Updated header using Bootstrap

--- components/NavbarHeader.tsx
+++ components/NavbarHeader.tsx
@@ -1,16 +1,37 @@
-// import {Navbar, Nav} from 'react-bootstrap';
+import {Image, Navbar, Nav} from 'react-bootstrap';
+// import Image from 'next/image';
 import Link from 'next/link';
 
+
 const NavbarHeader = () => {
+
     return (
-        <div className="header">
-        <Link href="/"><a className="logo">C & C Pressure Washing</a></Link>
-        <div className="header-right">
-            <Link href="/"><a>Home</a></Link>
-            <Link href="/contact"><a>Contact</a></Link>
-            <Link href="/about"><a>About</a></Link>
-        </div>
-        </div>
+        // <div className="header">
+        // <Link href="/"><a><Image className="logo" src="/images/CC.png" width="75px" height="75px"/></a></Link>
+        // <div className="header-right">
+        //     <Link href="/"><a>Home</a></Link>
+        //     <Link href="/contact"><a>Contact</a></Link>
+        //     <Link href="/about"><a>About</a></Link>
+        // </div>
+        // </div>
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="/">
+                <Image
+                        alt=""
+                        src="/images/CC.png"
+                        width="75"
+                        height="75"
+                        className="d-inline-block align-top"
+                    />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav>
+                <Nav.Link href="/about">About</Nav.Link>
+                <Nav.Link href="/contact">Contact</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
 
